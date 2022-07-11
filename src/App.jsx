5ec00d04906a6d1cec24c44e129a856d642cf4b8
Maria Baykova <mnbaikova@gmail.com>
2022-07-11 13:03:13 +0500

    some structure created, can be changed later

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Header from "./assets/components/Header";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header title="header title" />
+    </div>
+  );
+}
+
+export default App;
